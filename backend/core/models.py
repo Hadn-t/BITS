9fc4624bdb107b3d97b663afbf03fa2cefe30b64
nishu-02 <0264nishu@gmail.com>
2025-01-16 20:16:14 +0530

    Files model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class FileUpload(models.Model):
+    CATEGORY_CHOICES = [
+        ('prescription', 'Prescription'),
+        ('lab_result', 'Lab Result'),
+    ]
+
+    user_id = models.charField(max_length=150)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    file = models.FileField(upload_to='uploads/%Y/%m/%d')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.category} - {self.user_id}"    
