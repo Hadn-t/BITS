abea1a8e4538492c1562b63621239b852fccc419
Altairian7 <artisharma.as12.as@gmail.com>
2025-01-18 08:53:46 +0530

    added a get request function and index.html to core

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import upload_file, get_user_files
+from .views import upload_file, get_user_files, Index
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+    path('', Index.as_view(), name='index'),
     path('upload/', upload_file, name='file-upload'),
     path('user/<str:user_id>/files/', get_user_files, name='get_user_files'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
