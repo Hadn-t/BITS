668bd5a73d1e9ddb7acc6c3231f868dba219892c
Altairian7 <artisharma.as12.as@gmail.com>
2025-01-18 07:05:49 +0530

    connected postgres to the backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hospitalDB',
+        'USER': 'postgres',
+        'PASSWORD': '48526',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
