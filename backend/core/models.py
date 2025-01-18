f8a36c21a36cab13bcf78b02f3faa879456a77fe
Altairian7 <artisharma.as12.as@gmail.com>
2025-01-18 09:20:30 +0530

    created doctor profile and called it in admin

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 class FileUpload(models.Model):
     CATEGORY_CHOICES = [
@@ -15,3 +15,21 @@ class FileUpload(models.Model):
 
     def __str__(self):
         return f"{self.category} - {self.user_id}"    
+    
+    
+User = get_user_model()
+
+class DoctorProfile(models.Model):
+    user_name= models.ForeignKey(User, on_delete=models.CASCADE)
+    name= models.TextField()
+    doctor_type= models.TextField()
+    hospital= models.CharField(max_length=50)
+    Education= models.CharField(max_length=20)
+    age= models.IntegerField()
+    hometown= models.TextField(blank=True)
+    
+    
+    
+    def __str__(self):
+        return self.user_name
+    
